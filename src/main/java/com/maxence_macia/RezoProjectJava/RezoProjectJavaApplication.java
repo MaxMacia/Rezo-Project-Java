22fca010e42f459cffc6e01bb26963d584b26991
MaxMacia <maxence.macia@gmail.com>
2023-07-18 11:50:49 +0200

    first commit, setup du project avec Spring boot, configuration du fichier application.properties, untraccking de application.properties

---
+++ src/main/java/com/maxence_macia/RezoProjectJava/RezoProjectJavaApplication.java
@@ -0,0 +1,13 @@
+package com.maxence_macia.RezoProjectJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RezoProjectJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RezoProjectJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maxence_macia/RezoProjectJava/RezoProjectJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maxence_macia.RezoProjectJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RezoProjectJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
