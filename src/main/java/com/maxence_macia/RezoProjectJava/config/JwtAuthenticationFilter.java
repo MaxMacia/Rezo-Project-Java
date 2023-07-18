b06308849bc6ef823763e9a55535e97ab9dda144
MaxMacia <maxence.macia@gmail.com>
2023-07-18 14:31:58 +0200

    début création de la classe JwtAuthenticationFilter qui va servir de filtre et verifier les tokens, début création classe JwtService pour gérer les tokens

---
+++ src/main/java/com/maxence_macia/RezoProjectJava/config/JwtAuthenticationFilter.java
@@ -0,0 +1,40 @@
+package com.maxence_macia.RezoProjectJava.config;
+
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.lang.NonNull;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.maxence_macia.RezoProjectJava.services.JwtService;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+	@Autowired
+	private JwtService jwtService;
+
+	@Override
+	protected void doFilterInternal(
+			@NonNull HttpServletRequest request,
+			@NonNull HttpServletResponse response,
+			@NonNull FilterChain filterChain
+			)
+			throws ServletException, IOException {
+		final String authHeader = request.getHeader("Authaurization");
+		final String jwt;
+		final String userLogin;
+		
+		if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+			filterChain.doFilter(request, response);
+			return;
+		}
+		
+		jwt = authHeader.substring(7);
+		userLogin = this.jwtService.extractName(jwt);
+	}
+
+}
---
+++ src/main/java/com/maxence_macia/RezoProjectJava/services/JwtService.java
@@ -0,0 +1,9 @@
+package com.maxence_macia.RezoProjectJava.services;
+
+public class JwtService {
+
+	public String extractName(String jwt) {
+		return null;
+	}
+
+}
