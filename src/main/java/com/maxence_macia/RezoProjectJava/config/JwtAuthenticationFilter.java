29ddca2a8489880eabfed5da32cb573e214b6ac7
MaxMacia <maxence.macia@gmail.com>
2023-07-18 15:59:46 +0200

    classe JwtAuthenticationFilter terminee

--- src/main/java/com/maxence_macia/RezoProjectJava/config/JwtAuthenticationFilter.java
+++ src/main/java/com/maxence_macia/RezoProjectJava/config/JwtAuthenticationFilter.java
@@ -4,9 +4,11 @@ import java.io.IOException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.lang.NonNull;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import com.maxence_macia.RezoProjectJava.services.JwtService;
@@ -45,7 +47,15 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 			UserDetails user = this.userDetailsService.loadUserByUsername(userLogin);
 			
 			if (this.jwtService.isTokenValid(jwt, user)) {
-				
+				UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
+						user,
+						null,
+						user.getAuthorities()
+						);
+				authToken.setDetails(
+						new WebAuthenticationDetailsSource().buildDetails(request)
+						);
+				SecurityContextHolder.getContext().setAuthentication(authToken);
 			}
 		}
 		filterChain.doFilter(request, response);
