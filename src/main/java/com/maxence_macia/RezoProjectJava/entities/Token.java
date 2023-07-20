19bd6641e9059ab5e338583c58cdcebbdd325771
MaxMacia <maxence.macia@gmail.com>
2023-07-20 09:15:06 +0200

    mapping de la classe Token et creation d'une relation ManyToOne avec la classe User

--- src/main/java/com/maxence_macia/RezoProjectJava/entities/Token.java
+++ src/main/java/com/maxence_macia/RezoProjectJava/entities/Token.java
@@ -1,18 +1,34 @@
 package com.maxence_macia.RezoProjectJava.entities;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="tokens")
 public class Token {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="id")
 	private long id;
+	@Column(name="token")
 	private String token;
+	@Enumerated(EnumType.STRING)
+	@Column(name="token_type")
 	private TokenType tokenType;
+	@Column(name="expired")
 	private boolean expired;
+	@Column(name="revoked")
 	private boolean revoked;
+	@ManyToOne
+	@JoinColumn(name="users_id")
+	private User user;
 	
 	public Token() {}
 	public Token(
 			String token,
 			TokenType tokenType,
 			boolean expired,
-			boolean revoked
+			boolean revoked,
+			User user
 			) {
 		super();
 		this.token = token;
@@ -50,5 +66,11 @@ public class Token {
 	}
 	public void setRevoked(boolean revoked) {
 		this.revoked = revoked;
+	}
+	public User getUser() {
+		return user;
+	}
+	public void setUser(User user) {
+		this.user = user;
 	}
 }
