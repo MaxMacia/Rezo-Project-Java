ba71111ff1d8fa20acbeaba28279e17cef4f7725
MaxMacia <maxence.macia@gmail.com>
2023-07-20 09:30:10 +0200

    creation d'une classe TokenRepository avec les methodes findAllValidTokenByUser() et findByToken()

---
+++ src/main/java/com/maxence_macia/RezoProjectJava/repositories/TokenRepository.java
@@ -0,0 +1,20 @@
+package com.maxence_macia.RezoProjectJava.repositories;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.maxence_macia.RezoProjectJava.entities.Token;
+
+public interface TokenRepository extends JpaRepository<Token, Long> {
+	@Query("""
+			select t from Token t inner join User u on t.user.id = u.id
+			where u.id = :userId and (t.expired = false or t.revoked = false)
+			""")
+	public List<Token> findAllValidTokenByUser(@Param("userId") long userId);
+	
+	public Optional<Token> findByToken(String token);
+}
