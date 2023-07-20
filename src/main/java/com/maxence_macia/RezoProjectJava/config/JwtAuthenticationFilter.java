8437e17b44a6b73f69e0238ced3d870ab42f3f4d
MaxMacia <maxence.macia@gmail.com>
2023-07-20 12:20:27 +0200

    ajout dans la methode doFilterInternal de la classz JwtAuthentificationFilter d'une variable isTokenValid pour controler si le token en base de onné est valide, ajout des methodes privées saveUserToken() et revokeAllUserTokens dans la classe Authentication se JwtAthenticationFilter d'une variable isTokenValid pour controler la validité du token en base de donné, ajout des méthodes privés saveUserToken() et revokeAllUserTokens() dans la classe AuthenticationService

--- src/main/java/com/maxence_macia/RezoProjectJava/config/JwtAuthenticationFilter.java
+++ src/main/java/com/maxence_macia/RezoProjectJava/config/JwtAuthenticationFilter.java
@@ -12,6 +12,7 @@ import org.springframework.security.web.authentication.WebAuthenticationDetailsS
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import com.maxence_macia.RezoProjectJava.repositories.TokenRepository;
 import com.maxence_macia.RezoProjectJava.services.JwtService;
 
 import jakarta.servlet.FilterChain;
@@ -25,6 +26,8 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 	private JwtService jwtService;
 	@Autowired
 	private UserDetailsService userDetailsService;
+	@Autowired
+	private TokenRepository tokenRepository;
 
 	@Override
 	protected void doFilterInternal(
@@ -47,8 +50,10 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 		
 		if (userLogin != null && SecurityContextHolder.getContext().getAuthentication() == null) {
 			UserDetails user = this.userDetailsService.loadUserByUsername(userLogin);
+			var isTokenValid = this.tokenRepository.findByToken(jwt)
+					.map(t -> !t.isExpired() && !t.isRevoked()).orElse(false);
 			
-			if (this.jwtService.isTokenValid(jwt, user)) {
+			if (this.jwtService.isTokenValid(jwt, user) && isTokenValid) {
 				UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
 						user,
 						null,
--- src/main/java/com/maxence_macia/RezoProjectJava/services/AuthenticationService.java
+++ src/main/java/com/maxence_macia/RezoProjectJava/services/AuthenticationService.java
@@ -10,8 +10,11 @@ import org.springframework.stereotype.Service;
 import com.maxence_macia.RezoProjectJava.auth.AuthenticationRequest;
 import com.maxence_macia.RezoProjectJava.auth.AuthenticationResponse;
 import com.maxence_macia.RezoProjectJava.auth.RegisterRequest;
+import com.maxence_macia.RezoProjectJava.entities.Token;
+import com.maxence_macia.RezoProjectJava.entities.TokenType;
 import com.maxence_macia.RezoProjectJava.entities.User;
 import com.maxence_macia.RezoProjectJava.exceptions.UserAlreadyExistException;
+import com.maxence_macia.RezoProjectJava.repositories.TokenRepository;
 import com.maxence_macia.RezoProjectJava.repositories.UserRepository;
 
 @Service
@@ -19,11 +22,14 @@ public class AuthenticationService {
 	@Autowired
 	private PasswordEncoder passwordEncoder;
 	@Autowired
-	private UserRepository repository;
+	private UserRepository userRepository;
+	@Autowired
+	private TokenRepository tokenRepository;
 	@Autowired
 	private JwtService jwtService;
 	@Autowired
 	private AuthenticationManager authenticationManager;
+	
 
 	public AuthenticationResponse register(RegisterRequest request) {
 		var user = new User();
@@ -32,11 +38,12 @@ public class AuthenticationService {
 		user.setPassword(this.passwordEncoder.encode(request.getPassword()));
 		user.setRole(request.getRole());
 		
-		var userInDB = this.repository.findByLogin(user.getLogin()).orElse(null);
+		var userInDB = this.userRepository.findByLogin(user.getLogin()).orElse(null);
 		if (userInDB != null) throw new UserAlreadyExistException("L'utilisateur existe déjà");
 		
-		this.repository.save(user);
+		var savedUser = this.userRepository.save(user);
 		var jwtToken = this.jwtService.generateToken(user);
+		this.saveUserToken(savedUser, jwtToken);
 		var response = new AuthenticationResponse();
 		response.setStatus(HttpStatus.CREATED.value());
 		response.setToken(jwtToken);
@@ -52,15 +59,49 @@ public class AuthenticationService {
 						request.getPassword()
 						)
 				);
-		var user = this.repository.findByLogin(request.getLogin())
+		var user = this.userRepository.findByLogin(request.getLogin())
 				.orElseThrow(() -> new UserAlreadyExistException("L'utilisateur existe déjà"));
 		var jwtToken = this.jwtService.generateToken(user);
+		this.revokeAllUserTokens(user);
+		this.saveUserToken(user, jwtToken);
 		var response = new AuthenticationResponse();
 		response.setStatus(HttpStatus.OK.value());
 		response.setToken(jwtToken);
 		response.setTimeStamp(System.currentTimeMillis());
 		
 		return response;
+	}
+	
+	private void revokeAllUserTokens(User user) {
+		var validUserTokens = this.tokenRepository.findAllValidTokenByUser(user.getId());
+		if (validUserTokens.isEmpty()) {
+			return;
+		}
+		validUserTokens.forEach(
+				t -> {
+					t.setExpired(true);
+					t.setRevoked(true);
+				}
+				);
+		this.tokenRepository.saveAll(validUserTokens);
+	}
+
+	private void saveUserToken(User user, String jwtToken) {
+		Token token = new Token();
+		token.setUser(user);
+		token.setToken(jwtToken);
+		token.setTokenType(TokenType.Bearer);
+		token.setExpired(false);
+		token.setRevoked(false);
+		this.tokenRepository.save(token);
+	}
+
+	public UserRepository getUserRepository() {
+		return userRepository;
+	}
+
+	public void setUserRepository(UserRepository userRepository) {
+		this.userRepository = userRepository;
 	}
 
 }
