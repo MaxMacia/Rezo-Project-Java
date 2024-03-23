10e0158ad42f2efa0de08794099e9eb354b9112b
MaxMacia <maxence.macia@gmail.com>
2024-03-23 10:32:47 +0100

    initial commit

---
+++ src/main/java/com/maxencemacia/Rezo/RezoApplication.java
@@ -0,0 +1,13 @@
+package com.maxencemacia.Rezo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RezoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RezoApplication.class, args);
+	}
+
+}
